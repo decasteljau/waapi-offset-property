5b1a7b19001eec2bb767cd47f4360a4f252414e3
Bernard Rodrigue <brodrigue@audiokinetic.com>
2018-09-26 15:52:37 -0400

    fixed formatting

--- offset_property.py
+++ offset_property.py
@@ -2,10 +2,14 @@
 import argparse
 from waapi import WaapiClient
 
-parser = argparse.ArgumentParser(description='Offset a property value for a list of objects. By default, it increases the volume of the specified objects.')
+parser = argparse.ArgumentParser(
+    description='Offset a property value for a list of objects. '
+    'By default, it increases the volume of the specified objects.')
 parser.add_argument('id', nargs='+', help='An object ID (GUID)')
-parser.add_argument("-p", "--property", default='Volume', help="specify the property name to modify (default=Volume)")
-parser.add_argument("-o", "--offset", type=float, default=1, help="specify the offset value to apply (default=1)")
+parser.add_argument("-p", "--property", default='Volume',
+                    help="specify the property name to modify (default=Volume)")
+parser.add_argument("-o", "--offset", type=float, default=1,
+                    help="specify the offset value to apply (default=1)")
 args = parser.parse_args()
 
 accessor = '@'+args.property
@@ -14,8 +18,8 @@ accessor = '@'+args.property
 with WaapiClient() as client:
 
     # Retrieve the volumes for the selected objects
-    query = { 'from': { 'id': args.id }}
-    options = { 'return': ['id', accessor]}
+    query = {'from': {'id': args.id}}
+    options = {'return': ['id', accessor]}
     result = client.call("ak.wwise.core.object.get", query, options=options)
 
     # Make sure all of our set property calls are regrouped undo a single undo
@@ -28,8 +32,8 @@ with WaapiClient() as client:
                 'object': object['id'],
                 'property': args.property,
                 'value': object[accessor] + args.offset
-                }
+            }
             client.call("ak.wwise.core.object.setProperty", setPropertyArgs)
 
-    client.call("ak.wwise.core.undo.endGroup",displayName='Offset '+args.property)
-
+    client.call("ak.wwise.core.undo.endGroup",
+                displayName='Offset '+args.property)
